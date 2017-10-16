15e066981bb7591a9f4f839f49232bf0f8b125c4
Yuyue Wang <yw1384@nyu.edu>
2017-10-16 16:37:41 -0400

    added assert(False) for more validity

--- test.py
+++ test.py
@@ -3,24 +3,28 @@ import chemkin
 def test_xmlErrors():
 	try:
 		chemkin.DataParser().parse_file('data/test_parse.xml') # no phase labels
+		assert(False)
 	except Exception as err:
 		assert(type(err)==Exception)
 
 def test_negA():
 	try:
 		chemikin.DataParser().parse_file("data/test_negA.xml")
+		assert(False)
 	except Exception as err:
 		assert(type(err)==Exception)
 
 def test_btype():
 	try:
 		chemikin.DataParser().parse_file("data/test_btype.xml")
+		assert(False)
 	except Exception as err:
 		assert(type(err)==Exception)
 
 def test_negk():
 	try:
 		chemikin.DataParser().parse_file("data/test_negk.xml")
+		assert(False)
 	except Exception as err:
 		assert(type(err)==Exception)
 
