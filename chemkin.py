37d8e298fadd58ff5e9a61150d0076a6d01fd8a7
Hongxiang Qiu <hongxiangqiu@gmail.com>
2017-10-16 17:02:42 -0400

    add DataParser documentation

--- chemkin.py
+++ chemkin.py
@@ -292,10 +292,27 @@ class Reaction():
         self.rate_coeff = rate_coeff
 
 class DataParser():
+    """
+    XML parser for reaction data xml files.
+    """
+    
     def __init__(self):
         pass
     
     def _parse_rate_coeff(self, root):
+        """
+        Parse 'rateCoeff' element and return RateCoeff 
+    
+        Arguments
+        ----------
+        root: Element
+            rateCoeff Element 
+            
+        Return
+        ----------
+        RateCoeff
+            RateCoeff object
+        """
         root = root.getchildren()[0]
         if root.tag == "Arrhenius":
             A = float(root.find('A').text)
@@ -317,6 +334,19 @@ class DataParser():
             raise NotImplementedError("{} rate coefficient is not supported".format(root.tag))
     
     def _get_bool(self, v):
+        """
+        Convert an xml string boolean to python bool
+        
+        Arguments
+        ----------
+        v: str
+            xml string boolean
+        
+        Return
+        ----------
+        bool
+            boolean value
+        """
         v = str(v).lower()
         if v == 'yes' or v == 'true' or v == '1':
             return True
@@ -324,15 +354,41 @@ class DataParser():
             return False
     
     def _get_reactants(self, root):
+        """
+        Parse 'reactants' or 'products' element and return dictionary of reactants/products 
+    
+        Arguments
+        ----------
+        root: Element
+            reactants or products Element 
+            
+        Return
+        ----------
+        dict
+            a string to float array where keys are name of reactants/products and value being their Stoichiometric coefficients
+        """
         s = root.text.strip()
         reactants = s.split()
         result = {}
         for r in reactants:
             parts = r.split(':')
-            result[parts[0]]=parts[1]
+            result[parts[0]]=float(parts[1])
         return result
     
     def _parse_reaction(self, root):
+        """
+        Parse 'reaction' element and return Reaction 
+    
+        Arguments
+        ----------
+        root: Element
+            reaction Element 
+            
+        Return
+        ----------
+        Reaction
+            Reaction object
+        """
         rate_coeff = self._parse_rate_coeff(root.find('rateCoeff'))
         reversible = self._get_bool(root.get('reversible'))
         type_ = root.get('type')
@@ -342,6 +398,19 @@ class DataParser():
         return Reaction(id, reversible=reversible, type_=type_, reactants=reactants, products=products, rate_coeff=rate_coeff)
     
     def parse_file(self, filename):
+        """
+        Parse a reaction xml file and return ReactionData object
+    
+        Arguments
+        ----------
+        filename: str
+            filename of reaction xml file 
+            
+        Return
+        ----------
+        ReactionData
+            parsed ReactionData object
+        """
         import xml.etree.ElementTree as ET
         tree = ET.parse(filename)
         id = tree.find("reactionData").get("id")
