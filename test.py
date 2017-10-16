db50e7063730256956d706cebbf7e3d2e1a0f4c2
syj19950301 <syj19950301@users.noreply.github.com>
2017-10-16 17:27:21 -0400

    fix typo

--- test.py
+++ test.py
@@ -5,52 +5,52 @@ def test_xmlErrors():
 		chemkin.DataParser().parse_file('data/test_parse.xml') # no phase labels
 		assert(False)
 	except Exception as err:
-		assert(type(err)==Exception)
+		assert(True)
 
 def test_negA():
 	try:
-		chemikin.DataParser().parse_file("data/test_negA.xml")
+		chemkin.DataParser().parse_file("data/test_negA.xml")
 		assert(False)
 	except Exception as err:
-		assert(type(err)==Exception)
+		assert(True)
 
 def test_btype():
 	try:
-		chemikin.DataParser().parse_file("data/test_btype.xml")
+		chemkin.DataParser().parse_file("data/test_btype.xml")
 		assert(False)
 	except Exception as err:
-		assert(type(err)==Exception)
+		assert(True)
 
 def test_Atype():
 	try:
-		chemikin.DataParser().parse_file("data/test_Atype.xml")
+		chemkin.DataParser().parse_file("data/test_Atype.xml")
 		assert(False)
 	except Exception as err:
-		assert(type(err)==Exception)
+		assert(True)
 
 def test_negk():
 	try:
-		chemikin.DataParser().parse_file("data/test_negk.xml")
+		chemkin.DataParser().parse_file("data/test_negk.xml")
 		assert(False)
 	except Exception as err:
-		assert(type(err)==Exception)
+		assert(True)
 
 def test_errorChem():
 	try:
-		chemikin.DataParser().parse_file("data/test_errorChem.xml")
+		chemkin.DataParser().parse_file("data/test_errorChem.xml")
 		assert(False)
 	except Exception as err:
-		assert(type(err)==Exception)
+		assert(True)
 
 def test_oneMoreReaction():
 	try:
-		chemikin.DataParser().parse_file("data/test_oneMoreReaction.xml")
+		chemkin.DataParser().parse_file("data/test_oneMoreReaction.xml")
 	except Exception as err:
 		assert(False)
 
 def test_idCollision():
 	try:
-		chemikin.DataParser().parse_file("data/test_idCollision.xml")
+		chemkin.DataParser().parse_file("data/test_idCollision.xml")
 	except Exception as err:
 		assert(False)
 
