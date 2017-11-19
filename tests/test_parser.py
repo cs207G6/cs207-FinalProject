cc386570de668e2dd4068b364486cf851210a614
Hongxiang Qiu <hongxiangqiu@gmail.com>
2017-11-19 14:54:03 -0500

    fix tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,14 +1,21 @@
 from chemkin.parser import DataParser
+from chemkin.nasa import NASACoeffs
 from os.path import join
+
+nasa = NASACoeffs()
 
 
 def get_example_data_file(file):
     return join("chemkin/example_data", file)
 
 
+def parser_file(file_name):
+    return DataParser().parse_file(get_example_data_file(file_name), nasa)
+
+
 def test_xmlErrors():
     try:
-        DataParser().parse_file(get_example_data_file('test_parse.xml'))  # no phase labels
+        parser_file('test_parse.xml')  # no phase labels
         assert False
     except Exception:
         assert True
@@ -16,7 +23,7 @@ def test_xmlErrors():
 
 def test_negA():
     try:
-        DataParser().parse_file(get_example_data_file("test_negA.xml"))
+        parser_file("test_negA.xml")
         assert False
     except Exception:
         assert True
@@ -24,7 +31,7 @@ def test_negA():
 
 def test_btype():
     try:
-        DataParser().parse_file(get_example_data_file("test_btype.xml"))
+        parser_file("test_btype.xml")
         assert False
     except Exception:
         assert True
@@ -32,7 +39,7 @@ def test_btype():
 
 def test_Atype():
     try:
-        DataParser().parse_file(get_example_data_file("test_Atype.xml"))
+        parser_file("test_Atype.xml")
         assert False
     except Exception:
         assert True
@@ -40,7 +47,7 @@ def test_Atype():
 
 def test_negk():
     try:
-        DataParser().parse_file(get_example_data_file("test_negk.xml"))
+        parser_file("test_negk.xml")
         assert False
     except Exception:
         assert True
@@ -48,7 +55,7 @@ def test_negk():
 
 def test_errorChem():
     try:
-        DataParser().parse_file(get_example_data_file("test_errorChem.xml"))
+        parser_file("test_errorChem.xml")
         assert False
     except Exception:
         assert True
@@ -56,25 +63,25 @@ def test_errorChem():
 
 def test_oneMoreReaction():
     try:
-        DataParser().parse_file(get_example_data_file("test_oneMoreReaction.xml"))
+        parser_file("test_oneMoreReaction.xml")
     except Exception:
         assert False
 
 
 def test_length():
-    assert (len(DataParser().parse_file(get_example_data_file('rxns.xml'))) == 3)
+    assert (len(parser_file('rxns.xml')) == 3)
 
 
 def test_idCollision():
     try:
-        DataParser().parse_file(get_example_data_file("test_idCollision.xml"))
+        parser_file("test_idCollision.xml")
         assert False
     except Exception:
         assert True
 
 
 def test_progRateWrongDimension():
-    cd = DataParser().parse_file(get_example_data_file('rxns.xml'))
+    cd = parser_file('rxns.xml')
     try:
         cd.get_progress_rate([1, 2, 3, 4, 5], 100)
         assert False
@@ -84,7 +91,7 @@ def test_progRateWrongDimension():
 
 def test_progRateWrongSp():
     try:
-        DataParser().parse_file(get_example_data_file('test_wrongSp.xml'))
+        parser_file('test_wrongSp.xml')
         assert False
     except Exception:
         assert True
@@ -92,14 +99,14 @@ def test_progRateWrongSp():
 
 def test_progRateWrongSp2():
     try:
-        DataParser().parse_file(get_example_data_file('test_wrongSp2.xml'))
+        parser_file('test_wrongSp2.xml')
         assert False
     except Exception:
         assert True
 
 
 def test_progRateNonIrr():
-    cd = DataParser().parse_file(get_example_data_file('test_nonirr.xml'))
+    cd = parser_file('test_nonirr.xml')
     try:
         cd.get_progress_rate([1, 2, 3, 4, 5, 6], 100)
         assert False
@@ -108,9 +115,44 @@ def test_progRateNonIrr():
 
 
 def test_progRateNonEle():
-    cd = DataParser().parse_file(get_example_data_file('test_nonele.xml'))
+    cd = parser_file('test_nonele.xml')
     try:
         cd.get_progress_rate([1, 2, 3, 4, 5, 6], 100)
+        assert False
+    except Exception:
+        assert True
+
+
+def test_mixreverse():
+    cd = parser_file('test_mixreverse.xml')
+    try:
+        cd.get_progress_rate([1, 2, 3, 4, 5, 6, 7, 8], 100)
+        assert (True)
+    except Exception as err:
+        assert (False)
+
+
+def test_allreverse():
+    try:
+        cd = parser_file('test_allreverse.xml')
+        cd.get_progress_rate([1, 2, 3, 4, 5, 6, 7, 8], 100)
+        assert (True)
+    except Exception as err:
+        assert (False)
+
+
+def test_RxWtWrongSp():
+    try:
+        cd = parser_file('test_RxWtWrongSp.xml')
+        cd.get_progress_rate([1, 2, 3, 4, 5, 6, 7, 8], 100)
+        assert (False)
+    except Exception as err:
+        assert (True)
+
+
+def test_WrongSp_in_array():
+    try:
+        parser_file('test_WrongSp_in_array.xml')
         assert False
     except Exception:
         assert True
