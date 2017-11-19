7a47c7a2e7454658f314f33fec96e1dbe2b0e9bb
Yuyue Wang <yw1384@nyu.edu>
2017-11-19 13:40:29 -0500

    added test for reaction with wrong species

--- tests/chemkin_test.py
+++ tests/chemkin_test.py
@@ -131,3 +131,11 @@ def test_allreverse():
 		assert(True)
 	except Exception as err:
 		assert(False)
+
+def test_RxWtWrongSp():
+	try:
+		cd = DataParser().parse_file('test_RxWtWrongSp.xml')
+		cd.get_progress_rate([1,2,3,4,5,6,7,8],100)
+		assert(False)
+	except Exception as err:
+		assert(True)
