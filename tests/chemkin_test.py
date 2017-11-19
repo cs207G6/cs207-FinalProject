70152b431f494ab83d50f4d722177e12ad702535
Yuyue Wang <yw1384@nyu.edu>
2017-11-19 12:51:02 -0500

    added mixed reverse and irreversable tests

--- tests/chemkin_test.py
+++ tests/chemkin_test.py
@@ -98,3 +98,10 @@ def test_progRateNonEle():
 	except Exception as err:
 		assert(True)
         
+def test_mixreverse():
+	cd = chemkin.DataParser().parse_file('data/test_mixreverse.xml')
+	try:
+		cd.get_progress_rate([1,2,3,4,5,6,7,8],100)
+		assert(True)
+	except Exception as err:
+		assert(False)
