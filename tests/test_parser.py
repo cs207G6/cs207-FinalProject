a654f61dba3595c4a6c729fd8aa36d4003efe7fe
Yijun Shen <yijunshen@Yijuns-MacBook-Pro.local>
2017-11-19 22:00:53 -0500

    test case non-exist sp

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -184,14 +184,21 @@ def test_unknown_coeff():
 
 def test_db():
     try: 
-        nasa.get_coeffs("H", 'high')[0] == np.array([3.282538 ,1.483088e-03 ,-7.579667e-07, 2.094706e-10, -2.167178e-14, -1088.457720, 5.453231])
+        nasa.get_coeffs("H", 'high')[0] == np.array([3.282538, 1.483088e-03, -7.579667e-07, 2.094706e-10, -2.167178e-14, -1088.457720, 5.453231])
         assert True
     except Exception:
         assert False
 
 def test_temp_range():
     try:
-        nasa.get_coeffs("H", 'middle')[0] == np.array([3.282538 ,1.483088e-03 ,-7.579667e-07, 2.094706e-10, -2.167178e-14, -1088.457720, 5.453231])
+        nasa.get_coeffs("H", 'middle')[0] == np.array([3.282538, 1.483088e-03, -7.579667e-07, 2.094706e-10, -2.167178e-14, -1088.457720, 5.453231])
+        assert False
+    except Exception:
+        assert True
+
+def test_undefined_sp():
+    try:
+        nasa.get_coeffs("S", 'high')[0] == np.array([3.282538, 1.483088e-03, -7.579667e-07, 2.094706e-10, -2.167178e-14, -1088.457720, 5.453231])
         assert False
     except Exception:
         assert True
