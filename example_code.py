ba445afc7ac26160825a1482bb65f98a8f9c6fec
Yijun Shen <yijunshen@Yijuns-MacBook-Pro.local>
2017-12-08 15:30:21 -0500

    example code for model doc

---
+++ example_code.py
@@ -0,0 +1,19 @@
+import chemkin.parser
+import chemkin.nasa
+
+nasa = chemkin.nasa.NASACoeffs()
+data_parser = chemkin.parser.DataParser()
+
+reaction_data = data_parser.parse_file("chemkin/example_thermo.xml")
+
+# Get Progress Rate
+progress_rates = reaction_data.get_progress_rate.get_progress_rate([1,2,3,4,5,6],100)
+print(progress_rates)
+
+# Get Reaction Rate
+reaction_rates = reaction_data.get_reaction_rate(progress_rates)
+print(reaction_rates)
+
+# Get Reaction Rate Coefficients
+ks = reaction_data.get_k(20)
+print(ks)
