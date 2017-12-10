aef19fdcd74786b4cbfb09502b5aafb860312e02
Hongxiang Qiu <hongxiangqiu@gmail.com>
2017-12-10 15:16:00 -0500

    add doctest

--- chemkin/time_evo.py
+++ chemkin/time_evo.py
@@ -10,11 +10,31 @@ class TimeEvo:
         self.file = h5py.File(file, 'r')
         self.scenarios = list(self.file.keys())
 
-    def plot(self, scenario):
+    def plot(self, scenario, pic_width=16, pic_length=10):
+        """
+
+        Parameters
+        ----------
+        scenario: str
+            name of the scenario
+        pic_width:
+            width of output picture
+        pic_length:
+            height of output picture
+
+        Returns
+        -------
+        str
+            base64 encoded image (png)
+
+        Examples
+        --------
+        >>> time_evo = TimeEvo("chemkin/example_data/detailed_profile.h5")
+        >>> time_evo.plot(time_evo.scenarios[0], 0.1, 0.1)
+        'iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAPYQAAD2EBqD+naQAAANJJREFUGJWFkDFOw0AQRZ+XtSxZ2m1oseg4AU0OgMQJ0tBt4dNQpqHmGG4pKGjjCwxyk8q7JFIIm6EIcUEiGGmKP3oavZki56zDMOCcoygKfpeqklICEVHg37bOOQBEhPcPZf70CsD89gqA7WbNor3DppQA8N5T5i9MVXN9WfP4MAMgxsiiBdM0zeSz3e0BqKw5cTUiMoXPfADLizOg934Ke9XD8Mz1NsYIQPv8xnK1+wFPOOzR8WUpmKqmKg33Nw3HBeM4EkKAvu///F8IQbuu028NBl10rRrWqQAAAABJRU5ErkJggg=='
+        """
         data = self.file[scenario + '/truth']
         time = self.file[scenario + '/time']
-        pic_width = 1200 // 75
-        pic_length = 800 // 75
         plt.figure(figsize=(pic_width, pic_length))
         plt.plot(time.value, data.value[:, -1], label="Temperature")
         plt.title("Temperature Evolution")
