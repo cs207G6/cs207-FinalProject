84b71b75ec0f3d7411247f37a7957f5493e0755b
Yuyue Wang <yw1384@nyu.edu>
2017-12-10 15:45:03 -0500

    one docstring added

--- chemkin/plot.py
+++ chemkin/plot.py
@@ -10,12 +10,54 @@ import numpy as np
 
 
 def range_data_collection(user_data, input_concentration, lower_T, upper_T, current_T):
-    """
-    user_data: Read data from website that user uploaded
-    input_concentration: The concentrations of species input by users
-    lower_T: The lower bound of temperature range
-    upper_T: the upper bound of temperature range
-    current_T: The temperature of the current reaction
+    """Returns the base64-encoded plot for progress rates of multiple elementary reacitons vs temperature range
+
+        INPUTS:
+        =======
+        user_data:              ReactionData class object
+                                data of all elementary reactions from website that user uploaded
+        input_concentration:    python list of floats or integers
+                                The concentrations of each species given by users
+        lower_T:                float or integer
+                                The lower bound of temperature range
+        upper_T:                float or integer
+                                the upper bound of temperature range
+        current_T:              float or integer
+                                The temperature of the current reaction
+        
+        RETURNS:
+        ========
+        temp_range:             python list of 100 floats
+                                100 temperatures evenly spaced within the given temperature range
+        progress_rates_list:    python list of  100 numpy arrays
+                                100 progress rate arrays for 100 temperatures to be plotted respectively. Each array contains n floats 
+                                where n is the number of elementary reactions in the given system
+        reaction_rates_list:    python list of  100 numpy arrays
+                                100 reaction rate arrays for 100 temperatures to be plotted respectively. Each array contains n floats 
+                                where n is the number of species in the given system
+        current_T:              float or integer
+                                The temperature of the current reaction
+        species:                python list of strings
+                                names of all species in the given system
+        progress_rates_current: numpy array
+                                the progress rates for all elementary reactions of at the given current temperature
+        reaction_rates_current: numpy array
+                                the reaction rates for all species of at the given current temperature
+
+        EXAMPLES:
+        ========
+        >>> from .parser import DataParser
+        >>> from .nasa import NASACoeffs
+        >>> nasa = NASACoeffs() 
+        >>> data_parser = DataParser() 
+        >>> user_data = data_parser.parse_file('example_data/rxns.xml', nasa)
+        >>> input_concentration = [10,30,30,40,50,60]
+        >>> lower_T = 1000
+        >>> upper_T = 2000
+        >>> current_T = 150
+        >>> pic_width = 0.1
+        >>> pic_length = 0.1
+        (temp_range, progress_rates_list, reaction_rates_list, current_T, species, progress_rates_current, reaction_rates_current)=range_data_collection(user_data, input_concentration, lower_T, upper_T, current_T)
     """
 
     reaction_data = user_data
@@ -62,7 +104,10 @@ def progress_rate_plot_generation(T_range, progress_rate_range, current_T, progr
         y = [e[i] for e in progress_rate_range]
         plt.plot(x, y)
         plt.scatter(x, y, label='Elementary Reaction ' + str(i + 1))
-        plt.plot(curr_T, progress_rates_current[i], 'or',label='Current Temperature')
+        if i == 0:
+            plt.plot(curr_T, progress_rates_current[i], '^r',label='Current Temperature')
+        else:
+            plt.plot(curr_T, progress_rates_current[i], '^r')
     plt.xlabel("Temperature")
     plt.ylabel("Progress Rate")
     plt.title("Progress Rate vs Temperature by Reactions")
@@ -102,7 +147,10 @@ def reaction_rate_plot_generation(T_range, reaction_rate_range, current_T, react
         y = [e[i] for e in reaction_rate_range]
         plt.plot(x, y, label=None)
         plt.scatter(x, y, label=species[i])
-        plt.plot(curr_T, reaction_rates_current[i], 'or',label='Current Temperature')
+        if i == 0:
+            plt.plot(curr_T, reaction_rates_current[i], '^r',label='Current Temperature')
+        else:
+            plt.plot(curr_T, reaction_rates_current[i], '^r')
     plt.xlabel("Temperature")
     plt.ylabel("Reaction Rate")
     plt.title("Reaction Rate vs Temperature by Species")
@@ -114,3 +162,5 @@ def reaction_rate_plot_generation(T_range, reaction_rate_range, current_T, react
     figfile.seek(0)  # rewind to beginning of file
     figdata_png = base64.b64encode(figfile.getvalue())
     return figdata_png.decode('utf8')
+
+
